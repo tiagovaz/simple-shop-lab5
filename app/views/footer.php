6c2b4aab32fe2ed3eebf659bec11cb5063b9ec39
Tiago Bortoletto Vaz <tiago@debian.org>
2026-02-24 18:08:41 -0500

    Updating footer Lab5

--- app/views/footer.php
+++ app/views/footer.php
@@ -2,8 +2,9 @@
 <footer class="footer">
   <div class="container">
     <p class="muted">Second Hand Harmony</p>
-    <p class="muted" style="font-size: 12px; margin-top: 8px;">Vanier College - eCommerce - Lab4 - Winter 2026</p>
+    <p class="muted" style="font-size: 12px; margin-top: 8px;">Vanier College - eCommerce - Lab5 - Winter 2026</p>
   </div>
 </footer>
 </body>
+
 </html>
